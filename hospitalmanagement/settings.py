ce7a9c95848642199d51365e5a402b411ec3f9d8
HiteshSharma-github <85629794+HiteshSharma-github@users.noreply.github.com>
2022-03-26 14:12:32 +0530

    first commit

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -29,7 +29,7 @@ STATIC_URL = 'static'
 SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 LOGIN_REDIRECT_URL = '/afterlogin'
-
+ALLOWED_HOSTS = ['diagnosesastra.herokuapp.com', 'localhost', '127.0.0.1']
 # for contact us give your gmail id and password
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -151,3 +151,4 @@ EMAIL_HOST_PASSWORD = 'heloiamtheboss'  # host email password required
 # this process is required because google blocks apps authentication by default
 # email on which you will receive messages sent from website
 EMAIL_RECEIVING_USER = ['hiteshsharmahs0987@gmail.com']
+django_heroku.settings(locals())
